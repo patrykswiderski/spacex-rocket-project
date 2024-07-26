0e0d51e16ddb0a9ceae89fbe5ef92b946ea6c16f
Patryk <patryk.swiderski.85@gmail.com>
2024-07-26 16:39:12 +0200

    Bug fix

--- src/app/dashboard/_components/Header.tsx
+++ src/app/dashboard/_components/Header.tsx
@@ -1,24 +1,13 @@
 import React from "react";
-import { TypeAnimation } from "react-type-animation";
 
 function Header() {
 	return (
 		<header>
 			<div className="w-[80vw] m-auto text-center flex flex-col">
-				<TypeAnimation
-					sequence={[
-						"Welcome to SpaceX",
-						2000,
-						"SpaceX Garage",
-						2000,
-						"Explore SpaceX Rockets",
-						2000,
-					]}
-					wrapper="span"
-					speed={40}
-					repeat={Infinity}
-					className="text-white font-sans font-semibold text-5xl pb-8"
-				/>
+				<h2 className="text-white font-sans font-semibold text-5xl pb-8">
+					Welcome to SpaceX
+				</h2>
+
 				<h2 className="text-neutral-300 font-sans text-2xl">
 					SpaceX designs, manufactures and launches advanced rockets and
 					spacecraft. The company was founded in 2002 to revolutionize space
